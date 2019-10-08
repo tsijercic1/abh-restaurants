2cdd10a081884f74293e136eb9dbf59f5a421f68
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2019-10-08 16:11:24 +0200

    Email validation

--- webapp/src/main/java/com/atlantbh/devdays/demo/abh/restaurants/web/controller/auth/AuthenticationFilter.java
+++ webapp/src/main/java/com/atlantbh/devdays/demo/abh/restaurants/web/controller/auth/AuthenticationFilter.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -47,7 +48,12 @@ public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter
       HttpServletRequest request, HttpServletResponse response)
       throws AuthenticationException, IOException {
     AuthenticationRequest authenticationRequest = getAuthenticationRequest(request);
-
+    // validate email
+    String email = authenticationRequest.getEmail();
+    if (!email.matches(
+        "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
+      throw new BadCredentialsException("Email is not valid!");
+    }
     UsernamePasswordAuthenticationToken authRequest =
         new UsernamePasswordAuthenticationToken(
             authenticationRequest.getEmail(), authenticationRequest.getPassword());
